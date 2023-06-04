cf5305de7ff28fd6093abba567045a87eaade23e
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-06-04 17:25:34 +0200

    tests: run mongodb for all tests

---
+++ test/global-mocha-setup.js
@@ -0,0 +1,19 @@
+const { MongoMemoryServer } = require('mongodb-memory-server')
+
+let mongod
+
+before(async function () {
+    // This will create an new instance of "MongoMemoryServer" and automatically start it
+    mongod = await MongoMemoryServer.create({
+        instance: {
+            port: 27017
+        },
+        binary: {
+            version: '6.0.0'
+        }
+    })
+})
+
+after(function () {
+    return mongod.stop()
+})
