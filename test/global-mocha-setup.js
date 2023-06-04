49e38e288fbfc77901390244079eac308508a8ce
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-06-04 17:48:14 +0200

    tests: bind to all ip adresses

--- test/global-mocha-setup.js
+++ test/global-mocha-setup.js
@@ -6,6 +6,7 @@ before(async function () {
     // This will create an new instance of "MongoMemoryServer" and automatically start it
     mongod = await MongoMemoryServer.create({
         instance: {
+            ip: '::,0.0.0.0',
             port: 27017
         },
         binary: {
