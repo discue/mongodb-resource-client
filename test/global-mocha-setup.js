a28d05880f51163fa6d446bdd6c8d7a985d55636
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-06-17 20:11:18 +0200

    tests:set up mongo replica set
    For transactions to work

--- test/global-mocha-setup.js
+++ test/global-mocha-setup.js
@@ -1,13 +1,20 @@
-const { MongoMemoryServer } = require('mongodb-memory-server')
+const { MongoMemoryReplSet } = require('mongodb-memory-server')
 
 let mongod
 
 before(async function () {
     // This will create an new instance of "MongoMemoryServer" and automatically start it
-    mongod = await MongoMemoryServer.create({
+    mongod = await MongoMemoryReplSet.create({
+        instanceOpts: [{
+            ip: '::,0.0.0.0',
+            port: 27017
+        }],
         instance: {
             ip: '::,0.0.0.0',
             port: 27017
+        },
+        replSet: {
+            count: 1
         },
         binary: {
             version: '6.0.0'
