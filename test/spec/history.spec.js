737ec2b01373301e17b76a78c91063f859882dbd
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-12-18 17:09:30 +0100

    feat(history): make possible to use parent collection also for history

--- test/spec/history.spec.js
+++ test/spec/history.spec.js
@@ -26,6 +26,114 @@ describe('History', () => {
         return mongoDbClient.close()
     })
 
+    describe('without dedicated collection', () => {
+        const eventEmitter = new EventEmitter()
+        const storage = new Storage({ url: 'mongodb://127.0.0.1:27021', collectionName: '_subscriptions', eventEmitter })
+        const history = new History({ url: 'mongodb://127.0.0.1:27021', collectionName: '_subscriptions', usageEventPrefix: storage.usageEventPrefix, eventEmitter })
+        history.listenForStorageEvents()
+
+        after(() => {
+            return storage.close()
+        })
+
+        after(() => {
+            return history.close()
+        })
+
+        describe('.create', () => {
+            it('events are handled and create a history element', async () => {
+                const id = uuid()
+                await storage.create(id, { my: 'ghost' })
+                const doc = await storage.get(id)
+                expect(doc.my).to.equal('ghost')
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find().toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(1)
+            })
+            it('events contain the created resource', async () => {
+                const id = uuid()
+                await storage.create(id, { my: 'ghost' })
+                const doc = await storage.get(id)
+                expect(doc.my).to.equal('ghost')
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find({ id }).toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(1)
+
+                const [create] = all.at(0).history
+                expect(create.resource.my).to.equal('ghost')
+            })
+            it('events contain correct action property', async () => {
+                const id = uuid()
+                await storage.create(id, { my: 'ghost' })
+                const doc = await storage.get(id)
+                expect(doc.my).to.equal('ghost')
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find({ id }).toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(1)
+
+                const [create] = all.at(0).history
+                expect(create.action).to.equal('create')
+            })
+        })
+
+        describe('.update', () => {
+            let id
+
+            beforeEach(async () => {
+                id = uuid()
+                await storage.create(id, { my: 'ghost' })
+                const doc = await storage.get(id)
+                expect(doc.my).to.equal('ghost')
+            })
+
+            it('events are handled and create a history element', async () => {
+                await storage.update([id], { hello: 'peter' })
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find({ id }).toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(2)
+            })
+
+            it('events contain correct action property', async () => {
+                await storage.update([id], { hello: 'peter' })
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find({ id }).toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(2)
+
+                const [create, update] = all.at(0).history
+                expect(create.action).to.equal('create')
+                expect(update.action).to.equal('update')
+            })
+
+            it('events contain the updated resource', async () => {
+                await storage.update([id], { hello: 'peter' })
+                await new Promise((resolve) => setTimeout(resolve, 200))
+
+                const coll = mongoDbClient.db().collection('_subscriptions')
+                const all = await coll.find({ id }).toArray()
+                expect(all).to.have.length(1)
+                expect(all.at(0).history).to.have.length(2)
+
+                const [, update] = all.at(0).history
+                expect(update.resource.hello).to.equal('peter')
+            })
+        })
+    })
+
     describe('configured via constructor', () => {
         const eventEmitter = new EventEmitter()
         const storage = new Storage({ url: 'mongodb://127.0.0.1:27021', collectionName: '_subscriptions', eventEmitter })
@@ -179,7 +287,7 @@ describe('History', () => {
             })
         })
     })
-    
+
     describe('enabled at runtime', () => {
         const storage = new Storage({ url: 'mongodb://127.0.0.1:27021', collectionName: '_subscriptions' })
 
