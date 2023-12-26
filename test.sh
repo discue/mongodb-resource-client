5dde0b5f076db9ef6ad55d1ab2b6ba5c9697c6dc
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-12-26 14:31:44 +0100

    feat: remove OT dependencies to support noop library

--- test.sh
+++ test.sh
@@ -9,3 +9,23 @@ npm run test
 export DSQ_MONGODB_USE_SHARED_DB_CLIENT=false
 npm run test
 
+# check whether we are connected to the internet 
+# before downloading dependencies
+resetDependencies() {
+    # reset tracing dependencies only if we are not running on GH
+    if [[ -z "${GITHUB_ACTIONS}" ]]; then
+        npm un @discue/open-telemetry-tracing
+        npm -D i @discue/open-telemetry-tracing
+    fi
+}
+
+curl --head www.google.com &>"/dev/null"
+if [[ "${?}" == 0 ]]; then
+    trap resetDependencies ERR EXIT
+
+    # test with noop tracing library now
+    npm i -D @discue/open-telemetry-tracing@npm:@discue/open-telemetry-tracing-noop
+    npm run test
+
+    resetDependencies
+fi
